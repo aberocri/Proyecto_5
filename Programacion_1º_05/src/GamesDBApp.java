0582c915bc7a3027f04b769d090a5787af39c763
ALberto <ALberto@Servidorl>
2015-05-14 14:27:10 +0200

    Signed-off-by: ALberto <ALberto@Servidorl>

---
+++ Programacion_1º_05/src/GamesDBApp.java
@@ -0,0 +1,13 @@
+import Vista.VistaApp;
+
+
+public class GamesDBApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VistaApp vistaApp = new VistaApp();
+		vistaApp.setVisible(true);
+		
+	}
+
+}
